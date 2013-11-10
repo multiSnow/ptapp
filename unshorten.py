e529ba6d96953fc8aa19348ec8a1a5a98fe1cde0
multisnow <infinity.blick.winkel@gmail.com>
2013-11-10 10:52:53 +0800

    simplify geturl and getloc; long invalid respondin unshorted

--- unshorten.py
+++ unshorten.py
@@ -19,41 +19,26 @@
 
 from json import loads
 from logging import info
-from threading import Thread
 from urlparse import urlparse,urlunparse
 from google.appengine.api.urlfetch import fetch
 
 from config import BITLY_LOGIN,BITLY_APIKEY,TIMEOUT
 
 def geturl(url):
-    def fetch_to_dict(dict_in):
-        respond=fetch(dict_in['url'],method='GET')
-        dict_in['text']=respond.content
-        return 0
-
-    dict_req=dict(url=url,text=None)
-    mt=Thread(target=fetch_to_dict,args=(dict_req,))
-    mt.daemon=True
-    mt.start()
-    mt.join(TIMEOUT)
-    if mt.isAlive():
-        info('Timeout in require {0}'.format(urlparse(url).netloc))
-    return dict_req['text']
+    try:
+        respond=fetch(url,method='GET',deadline=TIMEOUT)
+        return respond.content
+    except:
+        info('Error in GET {0}'.format(url))
+        return None
 
 def getloc(url):
-    def header_to_dict(dict_in):
-        respond=fetch(dict_in['url'],method='HEAD',follow_redirects=False)
-        dict_in['text']=respond.headers['location']
-        return 0
-
-    dict_req=dict(url=url,text=None)
-    mt=Thread(target=header_to_dict,args=(dict_req,))
-    mt.daemon=True
-    mt.start()
-    mt.join(TIMEOUT)
-    if mt.isAlive():
-        info('Timeout in require {0}'.format(urlparse(url).netloc))
-    return dict_req['text']
+    try:
+        respond=fetch(dict_inurl,method='HEAD',deadline=TIMEOUT)
+        return respond.headers['location']
+    except:
+        info('Error in HEAD {0}'.format(url))
+        return None
 
 def exp_bitly(url_in,text,url_replace):
     bitly_netloc='api-ssl.bitly.com'
@@ -61,7 +46,10 @@ def exp_bitly(url_in,text,url_replace):
     bitly_query='login={0}&apiKey={1}&shortUrl={2}'.format(BITLY_LOGIN,BITLY_APIKEY,url_in)
     bitly_respond=geturl(urlunparse(('https',bitly_netloc,bitly_path,'',bitly_query,'')))
     if bitly_respond:
-        url_in=loads(bitly_respond)['data']['expand'][0]['long_url']
+        try:
+            url_in=loads(bitly_respond)['data']['expand'][0]['long_url']
+        except:
+            info('invailid respond from bitly: {0}'.format(bitly_respond))
     return [url_in,text]
 
 def exp_googl(url_in,text,url_replace):
@@ -70,7 +58,10 @@ def exp_googl(url_in,text,url_replace):
     googl_query='shortUrl={0}'.format(url_in)
     googl_respond=geturl(urlunparse(('https',googl_netloc,googl_path,'',googl_query,'')))
     if googl_respond:
-        url_in=loads(googl_respond)['longUrl']
+        try:
+            url_in=loads(googl_respond)['longUrl']
+        except:
+            info('invailid respond from googl: {0}'.format(googl_respond))
     return [url_in,text]
 
 def exp_isgd(url_in,text,url_replace):
@@ -79,7 +70,10 @@ def exp_isgd(url_in,text,url_replace):
     isgd_query='shorturl={0}&format=json'.format(url_in)
     isgd_respond=geturl(urlunparse(('http',isgd_netloc,isgd_path,'',isgd_query,'')))
     if isgd_respond:
-        url_in=loads(isgd_respond)['url']
+        try:
+            url_in=loads(isgd_respond)['url']
+        except:
+            info('invailid respond from isgd: {0}'.format(isgd_respond))
     return [url_in,text]
 
 def exp_instaragm(url_in,text,url_replace):
@@ -88,7 +82,10 @@ def exp_instaragm(url_in,text,url_replace):
     instaragm_query='url={0}'.format(url_in)
     instaragm_respond=geturl(urlunparse(('https',instaragm_netloc,instaragm_path,'',instaragm_query,'')))
     if instaragm_respond:
-        url_in=loads(instaragm_respond)['url']
+        try:
+            url_in=loads(instaragm_respond)['url']
+        except:
+            info('invailid respond from instaragm: {0}'.format(instaragm_respond))
     return [url_in,text]
 
 def exp_tldg(url_in,text,url_replace):
@@ -104,9 +101,12 @@ def exp_tldg(url_in,text,url_replace):
     tldg_path='/api_read/{0}'.format(url_id)
     tldg_respond=geturl(urlunparse(('http',tldg_netloc,tldg_path,'','','')))
     if tldg_respond:
-        orig_text=fromstring(tldg_respond)['post']['content'].text
-        new_text=u'({0})『{1}』'.format(url_replace,orig_text)
-        text=text.replace(url_replace,new_text)
+        try:
+            orig_text=fromstring(tldg_respond)['post']['content'].text
+            new_text=u'({0})『{1}』'.format(url_replace,orig_text)
+            text=text.replace(url_replace,new_text)
+        except:
+            info('invailid respond from tldg: {0}'.format(tldg_respond))
     return [url_in,text]
 
 def exp_imgly(url_in,text,url_replace):
