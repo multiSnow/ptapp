7a95c25c96fb0dbdaa19b8ceec96ddd00e7ee1c6
multisnow <infinity.blick.winkel@gmail.com>
2013-11-18 22:22:48 +0800

    update main script

--- ptapp/main-private.py
+++ ptapp/main-private.py
@@ -17,14 +17,27 @@
 # TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-from webapp2 import RequestHandler,WSGIApplication
+from json import dumps
 from logging import debug
+from urllib import urlencode
 from urlparse import urlparse,urlunparse,parse_qsl
-from oauth import TwitterClient
+
+from webapp2 import RequestHandler,WSGIApplication
 
 from config import CONSUMER_KEY,CONSUMER_SECRET,SCREEN_NAME,ACCESS_TOKEN,ACCESS_TOKEN_SECRET,USER_PASSWORD,API_IMPROVE,FILTER
+from oauth import TwitterClient
 
-ptapp_message='''
+if FILTER==1:
+    from statusfilter import statusfilter
+else:
+    statusfilter=lambda t:t
+
+if API_IMPROVE==1:
+    from linkrewriter import linkrewriter
+else:
+    linkrewriter=lambda t:t
+
+dummy_msg='''
 <html><head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 <title>404 Not Found</title>
@@ -35,122 +48,71 @@ ptapp_message='''
 <h2></h2>
 </body></html>'''
 
+work_msg='''
+<html><head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<title>PTAPP is working</title>
+</head>
+<body>
+<h1>PTAPP is working</h1>
+<h2>PTAPP for {0} deployed on {1} is working now</h2>
+</body></html>
+'''
+
 class MainPage(RequestHandler):
 
-    def conver_url(self,orig_url):
-        (scm,netloc,path,params,query,_)=urlparse(orig_url)
-        
-        path_parts=path.split('/')
+    def api(self):
 
-        if path_parts[2]=='api':
-            path_parts=path_parts[3:]
-            path_parts.insert(0,'/')
-            new_path='/{0}'.format('/'.join(path_parts[1:]).replace('//','/'))
-            new_netloc='api.twitter.com'
-        elif path_parts[2]=='searchapi':
-            path_parts=path_parts[3:]
-            path_parts.insert(0,'/')
-            new_path='/{0}'.format('/'.join(path_parts[1:]).replace('//','/'))
-            new_netloc='search.twitter.com'
-        elif path_parts[2].startswith('search.json'):
-            new_path=path
-            new_netloc='search.twitter.com'
-        else:
-            new_path=path
-            new_netloc='api.twitter.com'
-
-        new_path=new_path.replace('/{0}/'.format(USER_PASSWORD),'/')
-        new_url=urlunparse(('https',new_netloc,new_path.replace('//','/'),params,query,''))
-        return new_url,new_path
-
-    def do_proxy(self,method):
-        orig_url=self.request.url
-        orig_body=self.request.body
-
-        new_url,new_path=self.conver_url(orig_url)
-
-        if new_path=='/' or new_path=='':
-            self.response.set_status(200)
+        path_list=self.request.path.split('/')
+        if path_list.pop(1)!=USER_PASSWORD:
             self.response.headers['Content-Type']='text/html'
-            self.response.write(ptapp_message.format(self.request.path))
-            return 0
-        
-        user_access_token=None
-        
-        callback_url='{0}/oauth/verify'.format(self.request.host_url)
-        client=TwitterClient(CONSUMER_KEY,CONSUMER_SECRET,callback_url)
+            self.response.set_status(404)
+            self.response.write(dummy_msg.format(self.request.path_qs))
+            return
+        elif path_list==[''] or path_list==['','']:
+            self.response.headers['Content-Type']='text/html'
+            self.response.write(work_msg.format(SCREEN_NAME,self.request.host))
+            return
+        elif path_list[2:]==['oauth','access_token']:
+            self.response.headers['Content-Type']='text/plain'
+            self.response.write(urlencode([('oauth_token',ACCESS_TOKEN),
+                                           ('oauth_token_secret',ACCESS_TOKEN_SECRET),
+                                           ('user_id',ACCESS_TOKEN.split('-')[0]),
+                                           ('screen_name',SCREEN_NAME),
+                                           ('x_auth_expires',0)]))
+            return
 
-        protected=True
-        user_access_token,user_access_secret=ACCESS_TOKEN,ACCESS_TOKEN_SECRET
-
-        additional_params=dict([(k,v) for k,v in parse_qsl(orig_body)])
-
-        use_method=method
-
+        client=TwitterClient(CONSUMER_KEY,CONSUMER_SECRET,None)
         try:
-            debug('Start communicate to twitter.')
-            data=client.make_request(url=new_url,
-                                     token=user_access_token,
-                                     secret=user_access_secret,
-                                     method=use_method,
-                                     protected=protected,
-                                     additional_params=additional_params)
-        except Exception,error_message:
+            data=client.make_request(url=urlunparse(('https','api.twitter.com','/'.join(path_list),None,self.request.query_string,None)),
+                                     token=ACCESS_TOKEN,
+                                     secret=ACCESS_TOKEN_SECRET,
+                                     method=request.method,
+                                     protected=True,
+                                     additional_params=dict([(k,v) for k,v in parse_qsl(self.request.body)]))
+        except Exception as error_message:
             debug(error_message)
-            from json import dumps
             self.response.set_status(503)
             self.response.headers['Content-Type']='application/json'
             self.response.write(dumps(dict(error=str(error_message)),separators=(',', ':')))
+            return
         else:
-            if data.status_code>=500:
-                self.response.headers['Content-Type']='application/json'
-                self.response.write('{"error":"Twitter / Over capacity"}')
-            elif data.status_code>=400:
-                self.response.headers['Content-Type']='application/json'
+            self.response.set_status(data.status_code)
+            self.response.headers['Content-Type']=data.headers['Content-Type']
+            if data.status_code>399:
                 self.response.write(data.content)
+                return
             else:
-                if new_path.endswith('.xml'):
-                    self.response.headers['Content-Type']='application/xml'
-                    self.response.write(data.content)
-                elif new_path.endswith('.json'):
+                if self.request.path.endswith('.json'):
                     status=data.content
-                    if method=='GET':
-                        if FILTER==1:
-                            debug('Filter start.')
-                            from statusfilter import statusfilter
-                            status=statusfilter(status)
-                            debug('Filter finished')
-                        if API_IMPROVE==1:
-                            debug('Rewriter start.')
-                            from linkrewriter import linkrewriter
-                            status=linkrewriter(status)
-                            debug('Rewrite Finished.')
-                    self.response.headers['Content-Type']='application/json'
+                    if self.request.method=='GET':
+                        status=statusfilter(status)
+                        status=linkrewriter(status)
                     self.response.write(status)
                 else:
                     self.response.write(data.content)
-        return 0
+        return
 
-    def post(self):
-        self.do_proxy('POST')
-    
-    def get(self):
-        self.do_proxy('GET')
+    get=post=lambda self:self.api()
 
-class OAuthPage(RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type']='text/plain'
-        self.response.write('oauth_token={0}&oauth_token_secret={1}&user_id={2}&screen_name={3}&x_auth_expires=0'.format(ACCESS_TOKEN,
-                                                                                                                         ACCESS_TOKEN_SECRET,
-                                                                                                                         ACCESS_TOKEN.split('-')[0],
-                                                                                                                         SCREEN_NAME))
-
-class DummyPage(RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type']='text/html'
-        self.response.set_status(404)
-        self.response.write(ptapp_message.format(self.request.path))
-
-app=WSGIApplication([('/{0}/oauth/access_token'.format(USER_PASSWORD),OAuthPage),
-                     ('/{0}/.*'.format(USER_PASSWORD),MainPage),
-                     ('/.*',DummyPage)],debug=True)
+app=WSGIApplication([('/.*',MainPage)],debug=True)
