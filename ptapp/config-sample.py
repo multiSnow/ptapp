2152f8044353cb2544344e709bfe3096a48adf81
multisnow <infinity.blick.winkel@gmail.com>
2014-01-01 20:22:03 +0800

    Happy New Year!

--- ptapp/config-sample.py
+++ ptapp/config-sample.py
@@ -1,7 +1,7 @@
 #coding: utf8
 # Project PTAPP
 #
-# Copyright (c) 2012, multiSnow <infinity.blick.winkel@gmail.com>
+# Copyright (c) 2014, multiSnow <infinity.blick.winkel@gmail.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for
 # any purpose with or without fee is hereby granted, provided that the
